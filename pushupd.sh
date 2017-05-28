3d8dfa320beecea236cb241784ff6db8048c7da5
root <root@GhostRider.GhostRider4274.g4.internal.cloudapp.net>
2017-05-28 18:44:32 +0000

    update push in : Sun May 28 18:44:32 UTC 2017

--- pushupd.sh
+++ pushupd.sh
@@ -3,7 +3,7 @@
 cd /opt/GhostRider
 #git pull origin master
 git pull
-/usr/local/bin/python3.4  new_xml_gen.py
+python3.4  new_xml_gen.py
 sleep 7
 git add *
 git commit -a -m "update push in : `date`"
